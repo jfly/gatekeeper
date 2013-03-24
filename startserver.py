6f791c1aff61f264c1b470ff1fe1123dc2f28f5e
Jeremy Fleischman <jeremyfleischman@gmail.com>
2013-03-24 12:42:40 -0700

    Now using new git-tools functionality.

---
+++ startserver.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import sys
+import subprocess
+gittools = __import__('git-tools')
+gittools.cdIntoScriptDir()
+
+extraCmds = subprocess.list2cmdline(sys.argv[1:])
+if extraCmds:
+   extraCmds = ' ' + extraCmds
+
+project = gittools.GitSensitiveProject(
+     name='gatekeeper',
+     compileCommand='npm install',
+     runCommand='node gatekeeper.js' + extraCmds)
+gittools.startGitSensitiveScreen('gatekeeper', [project])
--- startserver.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/bash
-
-node gatekeeper.js
